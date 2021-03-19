a36270f2b8a72a6a29709de915e33fdb218aa231
angela valle <angelamvc04@gmail.com>
2021-03-19 10:47:39 -0600

    primer commit

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,486 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.codigo = New System.Windows.Forms.TextBox()
+        Me.nombre = New System.Windows.Forms.TextBox()
+        Me.edad = New System.Windows.Forms.TextBox()
+        Me.genero = New System.Windows.Forms.TextBox()
+        Me.modalidad = New System.Windows.Forms.TextBox()
+        Me.num1 = New System.Windows.Forms.TextBox()
+        Me.num2 = New System.Windows.Forms.TextBox()
+        Me.num3 = New System.Windows.Forms.TextBox()
+        Me.suma = New System.Windows.Forms.TextBox()
+        Me.resta = New System.Windows.Forms.TextBox()
+        Me.producto = New System.Windows.Forms.TextBox()
+        Me.cociente = New System.Windows.Forms.TextBox()
+        Me.longn = New System.Windows.Forms.TextBox()
+        Me.long10 = New System.Windows.Forms.TextBox()
+        Me.seno = New System.Windows.Forms.TextBox()
+        Me.coseno = New System.Windows.Forms.TextBox()
+        Me.raiz = New System.Windows.Forms.TextBox()
+        Me.residuo = New System.Windows.Forms.TextBox()
+        Me.potencia = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.calcular = New System.Windows.Forms.Button()
+        Me.limpiar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'codigo
+        '
+        Me.codigo.Location = New System.Drawing.Point(196, 58)
+        Me.codigo.Name = "codigo"
+        Me.codigo.Size = New System.Drawing.Size(100, 20)
+        Me.codigo.TabIndex = 2
+        Me.codigo.Text = "1234"
+        '
+        'nombre
+        '
+        Me.nombre.Location = New System.Drawing.Point(196, 94)
+        Me.nombre.Name = "nombre"
+        Me.nombre.Size = New System.Drawing.Size(100, 20)
+        Me.nombre.TabIndex = 4
+        '
+        'edad
+        '
+        Me.edad.Location = New System.Drawing.Point(196, 131)
+        Me.edad.Name = "edad"
+        Me.edad.Size = New System.Drawing.Size(100, 20)
+        Me.edad.TabIndex = 6
+        '
+        'genero
+        '
+        Me.genero.Location = New System.Drawing.Point(196, 169)
+        Me.genero.Name = "genero"
+        Me.genero.Size = New System.Drawing.Size(100, 20)
+        Me.genero.TabIndex = 8
+        '
+        'modalidad
+        '
+        Me.modalidad.Location = New System.Drawing.Point(196, 209)
+        Me.modalidad.Name = "modalidad"
+        Me.modalidad.Size = New System.Drawing.Size(100, 20)
+        Me.modalidad.TabIndex = 10
+        '
+        'num1
+        '
+        Me.num1.Location = New System.Drawing.Point(196, 245)
+        Me.num1.Name = "num1"
+        Me.num1.Size = New System.Drawing.Size(100, 20)
+        Me.num1.TabIndex = 12
+        '
+        'num2
+        '
+        Me.num2.Location = New System.Drawing.Point(196, 286)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(100, 20)
+        Me.num2.TabIndex = 14
+        '
+        'num3
+        '
+        Me.num3.Location = New System.Drawing.Point(196, 325)
+        Me.num3.Name = "num3"
+        Me.num3.Size = New System.Drawing.Size(100, 20)
+        Me.num3.TabIndex = 16
+        '
+        'suma
+        '
+        Me.suma.Location = New System.Drawing.Point(630, 48)
+        Me.suma.Name = "suma"
+        Me.suma.Size = New System.Drawing.Size(100, 20)
+        Me.suma.TabIndex = 18
+        '
+        'resta
+        '
+        Me.resta.Location = New System.Drawing.Point(630, 83)
+        Me.resta.Name = "resta"
+        Me.resta.Size = New System.Drawing.Size(100, 20)
+        Me.resta.TabIndex = 20
+        '
+        'producto
+        '
+        Me.producto.Location = New System.Drawing.Point(630, 122)
+        Me.producto.Name = "producto"
+        Me.producto.Size = New System.Drawing.Size(100, 20)
+        Me.producto.TabIndex = 22
+        '
+        'cociente
+        '
+        Me.cociente.Location = New System.Drawing.Point(630, 170)
+        Me.cociente.Name = "cociente"
+        Me.cociente.Size = New System.Drawing.Size(100, 20)
+        Me.cociente.TabIndex = 24
+        '
+        'longn
+        '
+        Me.longn.Location = New System.Drawing.Point(630, 209)
+        Me.longn.Name = "longn"
+        Me.longn.Size = New System.Drawing.Size(100, 20)
+        Me.longn.TabIndex = 26
+        '
+        'long10
+        '
+        Me.long10.Location = New System.Drawing.Point(630, 245)
+        Me.long10.Name = "long10"
+        Me.long10.Size = New System.Drawing.Size(100, 20)
+        Me.long10.TabIndex = 28
+        '
+        'seno
+        '
+        Me.seno.Location = New System.Drawing.Point(630, 286)
+        Me.seno.Name = "seno"
+        Me.seno.Size = New System.Drawing.Size(100, 20)
+        Me.seno.TabIndex = 30
+        '
+        'coseno
+        '
+        Me.coseno.Location = New System.Drawing.Point(630, 326)
+        Me.coseno.Name = "coseno"
+        Me.coseno.Size = New System.Drawing.Size(100, 20)
+        Me.coseno.TabIndex = 32
+        '
+        'raiz
+        '
+        Me.raiz.Location = New System.Drawing.Point(630, 362)
+        Me.raiz.Name = "raiz"
+        Me.raiz.Size = New System.Drawing.Size(100, 20)
+        Me.raiz.TabIndex = 34
+        '
+        'residuo
+        '
+        Me.residuo.Location = New System.Drawing.Point(630, 399)
+        Me.residuo.Name = "residuo"
+        Me.residuo.Size = New System.Drawing.Size(100, 20)
+        Me.residuo.TabIndex = 36
+        '
+        'potencia
+        '
+        Me.potencia.Location = New System.Drawing.Point(630, 436)
+        Me.potencia.Name = "potencia"
+        Me.potencia.Size = New System.Drawing.Size(100, 20)
+        Me.potencia.TabIndex = 38
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(97, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 39
+        Me.Label1.Text = "Código"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(93, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 40
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(97, 138)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 41
+        Me.Label3.Text = "Edad"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(97, 176)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 13)
+        Me.Label4.TabIndex = 42
+        Me.Label4.Text = "Género"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(53, 209)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(109, 13)
+        Me.Label5.TabIndex = 43
+        Me.Label5.Text = "Modalidad de Estudio"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(84, 251)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(53, 13)
+        Me.Label6.TabIndex = 44
+        Me.Label6.Text = "Número 1"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(84, 289)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(53, 13)
+        Me.Label7.TabIndex = 45
+        Me.Label7.Text = "Número 2"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(82, 332)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(53, 13)
+        Me.Label8.TabIndex = 46
+        Me.Label8.Text = "Número 3"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(524, 57)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(34, 13)
+        Me.Label9.TabIndex = 47
+        Me.Label9.Text = "Suma"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(519, 86)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(35, 13)
+        Me.Label10.TabIndex = 48
+        Me.Label10.Text = "Resta"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(519, 125)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(50, 13)
+        Me.Label11.TabIndex = 49
+        Me.Label11.Text = "Producto"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(519, 165)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(49, 13)
+        Me.Label12.TabIndex = 50
+        Me.Label12.Text = "Cociente"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(519, 212)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(33, 13)
+        Me.Label13.TabIndex = 51
+        Me.Label13.Text = "LogN"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(522, 250)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(37, 13)
+        Me.Label14.TabIndex = 52
+        Me.Label14.Text = "Log10"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(522, 292)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(32, 13)
+        Me.Label15.TabIndex = 53
+        Me.Label15.Text = "Seno"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(522, 331)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(43, 13)
+        Me.Label16.TabIndex = 54
+        Me.Label16.Text = "Coseno"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(417, 365)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(175, 13)
+        Me.Label17.TabIndex = 55
+        Me.Label17.Text = "Raiz del producto de dicho número "
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(409, 439)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(183, 13)
+        Me.Label18.TabIndex = 56
+        Me.Label18.Text = "Potencia del núm.1 Elevado al núm.3"
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(438, 406)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(154, 13)
+        Me.Label19.TabIndex = 57
+        Me.Label19.Text = "Residuo del num.1 entre num.2"
+        '
+        'calcular
+        '
+        Me.calcular.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.calcular.Location = New System.Drawing.Point(56, 395)
+        Me.calcular.Name = "calcular"
+        Me.calcular.Size = New System.Drawing.Size(75, 23)
+        Me.calcular.TabIndex = 58
+        Me.calcular.Text = "Calcular"
+        Me.calcular.UseVisualStyleBackColor = False
+        '
+        'limpiar
+        '
+        Me.limpiar.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.limpiar.Location = New System.Drawing.Point(180, 395)
+        Me.limpiar.Name = "limpiar"
+        Me.limpiar.Size = New System.Drawing.Size(75, 23)
+        Me.limpiar.TabIndex = 59
+        Me.limpiar.Text = "Limpiar"
+        Me.limpiar.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(848, 483)
+        Me.Controls.Add(Me.limpiar)
+        Me.Controls.Add(Me.calcular)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.potencia)
+        Me.Controls.Add(Me.residuo)
+        Me.Controls.Add(Me.raiz)
+        Me.Controls.Add(Me.coseno)
+        Me.Controls.Add(Me.seno)
+        Me.Controls.Add(Me.long10)
+        Me.Controls.Add(Me.longn)
+        Me.Controls.Add(Me.cociente)
+        Me.Controls.Add(Me.producto)
+        Me.Controls.Add(Me.resta)
+        Me.Controls.Add(Me.suma)
+        Me.Controls.Add(Me.num3)
+        Me.Controls.Add(Me.num2)
+        Me.Controls.Add(Me.num1)
+        Me.Controls.Add(Me.modalidad)
+        Me.Controls.Add(Me.genero)
+        Me.Controls.Add(Me.edad)
+        Me.Controls.Add(Me.nombre)
+        Me.Controls.Add(Me.codigo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents codigo As System.Windows.Forms.TextBox
+    Friend WithEvents nombre As System.Windows.Forms.TextBox
+    Friend WithEvents edad As System.Windows.Forms.TextBox
+    Friend WithEvents genero As System.Windows.Forms.TextBox
+    Friend WithEvents modalidad As System.Windows.Forms.TextBox
+    Friend WithEvents num1 As System.Windows.Forms.TextBox
+    Friend WithEvents num2 As System.Windows.Forms.TextBox
+    Friend WithEvents num3 As System.Windows.Forms.TextBox
+    Friend WithEvents suma As System.Windows.Forms.TextBox
+    Friend WithEvents resta As System.Windows.Forms.TextBox
+    Friend WithEvents producto As System.Windows.Forms.TextBox
+    Friend WithEvents cociente As System.Windows.Forms.TextBox
+    Friend WithEvents longn As System.Windows.Forms.TextBox
+    Friend WithEvents long10 As System.Windows.Forms.TextBox
+    Friend WithEvents seno As System.Windows.Forms.TextBox
+    Friend WithEvents coseno As System.Windows.Forms.TextBox
+    Friend WithEvents raiz As System.Windows.Forms.TextBox
+    Friend WithEvents residuo As System.Windows.Forms.TextBox
+    Friend WithEvents potencia As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents Label17 As System.Windows.Forms.Label
+    Friend WithEvents Label18 As System.Windows.Forms.Label
+    Friend WithEvents Label19 As System.Windows.Forms.Label
+    Friend WithEvents calcular As System.Windows.Forms.Button
+    Friend WithEvents limpiar As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
+
+    End Sub
+
+    Private Sub Label19_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub calcular_Click(sender As Object, e As EventArgs) Handles calcular.Click
+        ' suma
+        suma.Text = Val(num1.Text) + Val(num2.Text) + Val(num3.Text)
+        ' resta
+        resta.Text = Val(num1.Text) - Val(num2.Text) - Val(num3.Text)
+        ' producto
+        producto.Text = Val(num1.Text) * Val(num2.Text) * Val(num3.Text)
+        ' cociente
+        cociente.Text = Val(num1.Text) / Val(num2.Text)
+        ' logN
+        longn.Text = Val(Math.Log(Val(num1.Text)))
+        ' log10
+        long10.Text = Val(Math.Log10(Val(num1.Text)))
+        ' seno
+        seno.Text = Val(Math.Sin(Val(num1.Text)))
+        ' cosceno
+        coseno.Text = Val(Math.Cos(Val(num1.Text)))
+        ' raiz
+        raiz.Text = Val(Math.Sqrt(Val(producto.Text)))
+
+        'reciduo
+        residuo.Text = Val(num1.Text) Mod Val(num2.Text)
+
+        'potencia 
+        potencia.Text = Val(Math.Pow(Val(num1.Text), Val(num3.Text)))
+
+
+
+    End Sub
+
+    Private Sub limpiar_Click(sender As Object, e As EventArgs) Handles limpiar.Click
+        ' limpiar
+        num1.Clear()
+        num2.Clear()
+        num3.Clear()
+        suma.Clear()
+        resta.Clear()
+        producto.Clear()
+        cociente.Clear()
+
+    End Sub
+
+ 
+End Class
